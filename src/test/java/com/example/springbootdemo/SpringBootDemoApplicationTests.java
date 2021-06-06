ffe6fb80b2f2fc62c4e3a637fa0d23e4f99e564f
unknown <xiedf.chn@gmail.com>
2021-06-06 18:37:04 +0100

    add application tests

--- src/test/java/com/example/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/example/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.springbootdemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private GreetingController controller;
+
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
 
 }
